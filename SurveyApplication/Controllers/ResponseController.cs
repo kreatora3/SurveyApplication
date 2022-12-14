305914e66afe34bb3504959832ae4b3f71da7f52
Ivan Tochev <annihilatora@gmail.com>
2022-12-14 15:22:56 +0200

    added controller and swagger

---
+++ SurveyApplication/Controllers/ResponseController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SurveyApplication.DAL.Models;
+using SurveyApplication.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveyApplication.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ResponseController : ControllerBase
+    {
+        private IResponseRepository repository;
+
+        public ResponseController(IResponseRepository responseRepository)
+        {
+            this.repository = responseRepository;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            var list = repository.Get();
+            return Ok(list);
+        }
+
+        [HttpPost("{id:int}")]
+        public ActionResult<FormResponse> Post(int id, [FromBody] FormResponse response)
+        {
+
+
+            // var rm = new FormResponse(){ Age= 15, Email = "dadaa@adada", FirstName ="Ivan", LastName="T", SurveyId=12};
+            repository.Add(response);
+
+            return Ok();
+        }
+    }
+}
---
+++ SurveyApplication/Controllers/SurveysController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SurveyApplication.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveyApplication.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SurveysController : ControllerBase
+    {
+        private ISurveyRepository repository;
+
+        public SurveysController(ISurveyRepository surveyRepository)
+        {
+            this.repository = surveyRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(repository.GetSurveys());
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(repository.GetSurveyByID(id));
+        }
+    }
+}
